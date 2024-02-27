84fd5b5c212acc57a5686497f334d3d895a14efd
EsmeraldaColiaza <ezmeraldiithaa29@gmail.com>
2024-02-27 10:07:48 -0600

    nombre

---
+++ src/github/nombres.java
@@ -0,0 +1,13 @@
+package github;
+
+import java.util.Scanner;
+
+public class nombres {
+	public static void main(String[] args) {
+	System.out.println ("mi nombre es Esmeralda");
+	Scanner sc = new Scanner (System.in);
+	String nombre = sc. next();
+
+
+}
+}
